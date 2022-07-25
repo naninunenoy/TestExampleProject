185df9ce2ba582332b368d8e74d46b13a7987a2a
naninunenoy <naninuneno0411@gmail.com>
2022-07-25 22:18:56 +0900

    WithoutTestProject

---
+++ Assets/WithTestProject/SampleScene.cs
@@ -0,0 +1,54 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace WithoutTestProject
+{
+    /// <summary>
+    /// トグルのon/offに合わせた数値を十進数で表示する
+    /// </summary>
+    public class SampleScene : MonoBehaviour
+    {
+        [SerializeField] Text text;
+        [SerializeField] Button[] buttons;
+
+        int[] _bits = new int[4];
+
+        public void OnClickButton0()
+        {
+            _bits[0] = _bits[0] == 0 ? 1 : 0;
+            buttons[0].image.color = _bits[0] == 1 ? Color.red : Color.white;
+            UpdateText();
+        }
+
+        public void OnClickButton1()
+        {
+            _bits[1] = _bits[1] == 0 ? 1 : 0;
+            buttons[1].image.color = _bits[1] == 1 ? Color.red : Color.white;
+            UpdateText();
+        }
+
+        public void OnClickButton2()
+        {
+            _bits[2] = _bits[2] == 0 ? 1 : 0;
+            buttons[2].image.color = _bits[2] == 1 ? Color.red : Color.white;
+            UpdateText();
+        }
+
+        public void OnClickButton3()
+        {
+            _bits[3] = _bits[3] == 0 ? 1 : 0;
+            buttons[3].image.color = _bits[3] == 1 ? Color.red : Color.white;
+            UpdateText();
+        }
+
+        void UpdateText()
+        {
+            var number = 0;
+            if (_bits[3] == 1) number += 1;
+            if (_bits[2] == 1) number += 2;
+            if (_bits[1] == 1) number += 4;
+            if (_bits[0] == 1) number += 8;
+            text.text = number.ToString("D2");
+        }
+    }
+}
